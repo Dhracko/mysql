18863576240ce90c2c11ab8cc684f558792a8ed5
Dhracko <dracko@hotmail.co.uk>
2021-10-27 19:49:41 +0100

    first commit

---
+++ login_create.php
@@ -0,0 +1,63 @@
+<?php
+
+if (isset($_POST['submit'])) {
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+
+    $query = "INSERT INTO users(username, password) ";
+    $query .= "VALUES ('$username', '$password')";
+
+    $result = mysqli_query($connection, $query);
+
+    if(!$result) {
+        die('Query FAILED' . mysqli_error());
+    };
+
+
+}
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container-md">
+        <!-- <div class="row"> -->
+        <div class="col-sm-6">
+            <form action="login_create.php" method="POST">
+                <div class="form-group">
+                    <label class="form-label" for="username">Username</label>
+                    <input type="text" name="username" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label class="form-label" for="password">Password</label>
+                    <input type="password" name="password" class="form-control">
+                </div>
+                <br>
+                <input class="btn btn-primary" type="submit" name="submit" value="Submit">
+
+            </form>
+
+        </div>
+        <!-- </div> -->
+    </div>
+
+</body>
+
+</html>
---
+++ login_update.php
@@ -0,0 +1,57 @@
+<?php include "db.php";?>
+<?php include "functions.php";?>
+
+<?php
+if(isset($_POST['submit'])) {
+    UpdateTable();
+
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <!-- <div class="row"> -->
+        <div class="col-sm-6">
+            <form action="login_update.php" method="POST">
+                <div class="form-group">
+                    <label class="form-label" for="username">Username</label>
+                    <input type="text" name="username" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label class="form-label" for="password">Password</label>
+                    <input type="password" name="password" class="form-control">
+                </div>
+                <br>
+                <div class="form-group">
+
+                    <select name="id" id="">
+                        <?php
+                        showAllData()
+                        ?>
+                    </select>
+
+                </div>
+                <br>
+                <input class="btn btn-primary" type="submit" name="submit" value="Update">
+
+            </form>
+        </div>
+        <!-- </div> -->
+    </div>
+
+</body>
+
+</html>
