1c3cab28407e36664162070f208f7a150e147474
Dhracko <dracko@hotmail.co.uk>
2021-10-28 06:45:23 +0100

    Added security to username

--- functions.php
+++ functions.php
@@ -7,6 +7,9 @@ function createRows()
         $username = $_POST['username'];
         $password = $_POST['password'];
 
+        $username = mysqli_real_escape_string($connection,$username); //Added Security
+        $password = mysqli_real_escape_string($connection, $password); //cleans the code of special caracters
+
 
         $query = "INSERT INTO users(username, password) ";
         $query .= "VALUES ('$username', '$password')";
