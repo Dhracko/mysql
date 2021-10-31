a19b49e9c14a68fad1a5c2afd2c4e8b948828875
Dhracko <dracko@hotmail.co.uk>
2021-10-31 13:48:58 +0000

    Set time on cookie

--- cookies.php
+++ cookies.php
@@ -2,7 +2,7 @@
 
 $name = "SomeName";
 $value = 100;
-$expiration = time() + (60*60*24*7); // time in seconds eg (seconds*minutes*hours*7)=1 week
+$expiration = time() + (60*60*24*7); // time in seconds eg (seconds*minutes*hours*week)=1 week
 
 setcookie($name,$value,$expiration);
 
@@ -12,6 +12,17 @@ setcookie($name,$value,$expiration);
 
 
 <?php include "includes/header.php" ?>
+<?php
+
+if(isset($_COOKIE["SomeName"])) {  //Check if the Cookie exists
+
+    $someOne = $_COOKIE["SomeName"];
+    echo $someOne;
+} else {
+    $someOne = "";
+}
+
+?>
 
 
 <?php include "includes/footer.php" ?>
