dd3c7d114b7f5ea949447e41ae441d523e5d97c0
Dhracko <dracko@hotmail.co.uk>
2021-10-31 14:06:29 +0000

    Use of SESSION using cookies from page to page

---
+++ session1.php
@@ -0,0 +1,16 @@
+<?php session_start();
+
+$_SESSION['greeting'] = "Hello Student";
+
+?>
+
+
+
+<?php include "includes/header.php" ?>
+<?php
+
+
+?>
+
+
+<?php include "includes/footer.php" ?>
---
+++ session2.php
@@ -0,0 +1,16 @@
+<?php session_start();
+
+echo $_SESSION['greeting'];
+
+?>
+
+
+
+<?php include "includes/header.php" ?>
+<?php
+
+
+?>
+
+
+<?php include "includes/footer.php" ?>
