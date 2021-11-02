adade20581a8996b7a34a9ed8dc13d409f67b038
Dhracko <dracko@hotmail.co.uk>
2021-11-02 06:53:14 +0000

    create instances and change the values

--- class_car3.php
+++ class_car3.php
@@ -4,22 +4,35 @@ use Car as GlobalCar;
 
 class Car {
 
-    var $wheels = 4;
+    var $wheels = 4; //varibles
     var $hood = 1;
     var $engine = 1;
     var $doors = 4;
 
-    function MoveWheels(){
-        echo "Wheels move";
+    function MoveWheels(){ //method
+        
+        $this->wheels = 10; // $Car->wheels = 10;
+    }
+
+    function CreateDoors(){ //method
+        
+        $this->doors = 6; // $Car->doors = 6;
     }
 
 }
 
-$bmw = new Car();
-$merces_benz = new Car();
+$bmw = new Car(); // create an instance and assign it to a varible
+// $bmw->wheels = 8; //change the value of wheels
+$truck = new Car();
 
-$bmw->MoveWheels(); // call the funtion
-$merces_benz->MoveWheels();
+//$bmw->MoveWheels(); // Call the method
+echo $bmw->wheels . "<br>";
+echo $truck->wheels = 10 . "<br>"; //we assign the instance with a value of 10 for $truck
+echo $bmw->wheels . "<br>"; //the instance still have a value of 4
+
+echo $truck->doors . "<br>"; // = 4
+$truck->CreateDoors();
+echo $truck->doors; // = 6 as we change the value
 
 ?>
 
