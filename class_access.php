4a6a5931d6276e08cc536dedfc7d69a652428f82
Dhracko <dracko@hotmail.co.uk>
2021-11-06 14:05:04 +0000

    Data Access

--- class_access.php
+++ class_access.php
@@ -10,19 +10,29 @@ class Car
     private $engine = 1; // only available to this class
     var $doors = 4;
 
-    function showProperty() //subclass
-    { //method
+    // function showProperty() //subclass
+    // { //method
 
-      echo $this->hood; 
-    }
+    //   echo $this->hood; 
+    // }
 }
 
 $bmw = new Car(); // create an instance and assign it to a varible
-
-echo $bmw->wheels; // = 4
-
+//echo $bmw->wheels; // = 4
 // echo $bmw->hood; // produce and error as it's outside the class Car
-echo $bmw->showProperty();
+//echo $bmw->showProperty(); // Because it's inside a class showProperty
+$semi = new Semi();
+
+class Semi extends Car { // Subclass as it's extended from Car
+    function showProperty() 
+    {
+      echo $this->hood;
+      echo $this->engine; //error
+    }
+
+}
+echo $semi->showProperty();
+
 
 ?>
 
